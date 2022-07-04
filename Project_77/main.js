3f9161859fdfb5099e711b1e9e373cd356e1e7f9
Shantanu <98827369+standerd6@users.noreply.github.com>
2022-07-04 19:44:19 +0530

    Add files via upload

---
+++ Project_77/main.js
@@ -0,0 +1,15 @@
+var first_name = "Shantanu";
+var last_name = "Kapoor"
+var full_name =first_name.concat(last_name);
+console.log(full_name)
+
+var a = 10;
+var b = 5;
+var c =a+b;
+console.log(c)
+var d = a-b;
+console.log(d)
+var e =a*b;
+console.log(e)
+var s = a/b;
+console.log(s)
